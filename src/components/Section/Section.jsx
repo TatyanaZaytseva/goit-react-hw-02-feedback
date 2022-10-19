5eb283177af9e7c67ee2393cac7af90fa4d86e54
TatyanaZaytseva <tatyana.lytvyniuk@gmail.com>
2022-10-19 14:03:52 +0300

    step 3 done

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from 'components/Section/Section.module.css';
+
+export const Section = ({ title, children }) => (
+  <section className={css.section}>
+    <p className={css.title}>{title}</p>
+    {children}
+  </section>
+);
